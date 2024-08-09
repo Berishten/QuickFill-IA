049845eebd2b7eb3910a97454eb4c1e00834b802
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-08-09 01:18:11 -0400

    add initial firebase setup

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+//TODO: asignar configuracion correspondiente
+export default firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
