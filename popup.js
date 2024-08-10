bf3703852a03fd04c8927e22994b8dc1f9ad951d
Berishten <vepaes439@gmail.com>
2024-08-10 18:11:55 -0400

    Se crea vista simple y se externalizan estilos

--- popup.js
+++ popup.js
@@ -1,16 +1,9 @@
 let isFileUploaded = false;
-document.addEventListener("DOMContentLoaded", function () {
-	let ctxInput = document.getElementById("ctx");
+let ctxInput = null;
 
-	ctxInput.addEventListener("change", function () {
-		console.log("bucle");
-		const ctx = ctxInput.value;
-		chrome.runtime.sendMessage({ action: "save_data", ctx: ctx });
-	});
-	
-	chrome.runtime.sendMessage({ action: "get_data" }, function (response) {
-		ctxInput.value = response;
-	});
+document.addEventListener("DOMContentLoaded", function () {
+	showFileName();
+	setupContext();
 
 	document
 		.getElementById("responderFormulario")
@@ -23,3 +16,30 @@ document.addEventListener("DOMContentLoaded", function () {
 			});
 		});
 });
+
+function setupContext() {
+	ctxInput = document.getElementById("ctx");
+	ctxInput.addEventListener("change", function () {
+		console.log("bucle");
+		const ctx = ctxInput.value;
+		chrome.runtime.sendMessage({ action: "save_data", ctx: ctx });
+	});
+
+	chrome.runtime.sendMessage({ action: "get_data" }, function (response) {
+		ctxInput.value = response;
+	});
+}
+
+function showFileName() {
+	const fileInput = document.getElementById("fileInput");
+	const fileName = document.getElementById("fileName");
+
+	fileInput.addEventListener("change", function () {
+		const file = fileInput.files[0];
+		if (file) {
+			fileName.textContent = file.name;
+		} else {
+			fileName.textContent = "Elige tu archivo";
+		}
+	});
+}
