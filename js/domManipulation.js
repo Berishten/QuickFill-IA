762b9b49ed97f579191c55a127834cf1cc73befc
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-08-11 02:52:09 -0400

    toggle popup

--- js/domManipulation.js
+++ js/domManipulation.js
@@ -1,46 +1,62 @@
-let formOriginalStyles = {};
 let overlayActive = false;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "manipular_dom") {
         detectForm();
     }
-})
+});
 
-//TODO: eliminar elementos credos cuando corresponda
+//TODO: detect only valid form
+/**
+ * toggles an overlay and highlights forms on the webpage.
+ */
 function detectForm() {
+    if (!overlayActive) {
+        const forms = document.querySelectorAll("form");
 
-	// resetStyles()
-	const lastOverlay = document.getElementById("forms-overlay");
-	if (lastOverlay) {
-		lastOverlay.remove();
-	}
+        if (forms.length > 0) {
+            overlayActive = true;
 
-	const overlay = document.createElement('div');
-	overlay.setAttribute("id", "forms-overlay")
+            const overlay = document.createElement("div");
+            overlay.setAttribute("id", "forms-overlay");
 
-	// styles
-	overlay.style.position = 'fixed';
-	overlay.style.top = '0';
-	overlay.style.left = '0';
-	overlay.style.width = '100%';
-	overlay.style.height = '100%';
-	overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-	overlay.style.zIndex = '9999';
-	// add to body
-	document.body.appendChild(overlay);
+            // styles
+            overlay.style.position = "fixed";
+            overlay.style.top = "0";
+            overlay.style.left = "0";
+            overlay.style.width = "100%";
+            overlay.style.height = "100%";
+            overlay.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+            overlay.style.zIndex = "9999";
+            // add to body
+            document.body.appendChild(overlay);
 
-	const forms = document.querySelectorAll("form")
-	if (forms.length > 0) {
-		forms.forEach((form, index) => {
-			form.style.position = 'relative';
-			form.style.zIndex = '10000';
-			form.style.outline = '20px solid white';
-			form.style.backgroundColor = 'white'; // Opcional
+            forms.forEach((form, index) => {
+                form.style.position = "relative";
+                form.style.zIndex = "10000";
+                form.style.outline = "20px solid white";
+                form.style.backgroundColor = "white"; // Opcional
 
-			console.log(`Form ${index + 1}:`, form);
-		});
-	} else {
-		console.log("No forms found on this page.");
-	}
+                console.log(`Form ${index + 1}:`, form);
+            });
+        } else {
+        alert("No forms found on this page.");
+        }
+
+    } else {
+        overlayActive = false;
+
+        const lastOverlay = document.getElementById("forms-overlay");
+        if (lastOverlay) {
+        lastOverlay.remove();
+        }
+
+        const forms = document.querySelectorAll("form");
+        forms.forEach((form) => {
+        form.style.position = "";
+        form.style.zIndex = "";
+        form.style.outline = "";
+        form.style.backgroundColor = "";
+        });
+    }
 }
--- js/popup.js
+++ js/popup.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	document
 		.getElementById("responderFormulario")
 		.addEventListener("click", () => {
+			document.body.classList.toggle("shrink");
+
 			chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 				chrome.tabs.sendMessage(tabs[0].id, {
 					action: "seleccionar_formulario",
